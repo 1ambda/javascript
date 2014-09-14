b9495519a7c0e58196a575dd1cb9fecba2d6ae72
1ambda <1amb4a@gmail.com>
2014-09-14 22:07:23 +0900

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    env: {
+      test: {
+	NODE_ENV: 'test'
+      }
+    },
+    mochaTest: {
+      test: {
+	options: {
+	  reporter: 'spec'
+	},
+	src: 'test/**/*.js'
+      }
+    }
+  });
+
+  // load grunt-modules
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  // custom tasks
+  grunt.registerTask('test', ['env:test', 'mochaTest']);
+};
