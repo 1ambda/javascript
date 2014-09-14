9320a62f28f0fe47e9f8596aa46735358989545e
1ambda <1amb4a@gmail.com>
2014-09-15 00:57:10 +0900

    spy

--- test/spy.js
+++ test/spy.js
@@ -4,7 +4,9 @@ var sinon = require('sinon'),
     should = require('should');
 
 // custom modules
-var once = require('../lib/example').once;
+var example = require('../lib/example'),
+    once = example.once,
+    createUser = example.createUser;
 
 /*
  * A test py is a function that records
@@ -16,18 +18,66 @@ var once = require('../lib/example').once;
  */
 
 describe('spy', function() {
-  it('calls the original function only once', function() {
-    var callback = sinon.spy();
-    var proxy = once(callback);
-    proxy();
-    proxy();
+  describe('spy()', function() {
+    it('calls the original function only once', function() {
+      var callback = sinon.spy();
+      var proxy = once(callback);
+      proxy();
+      proxy();
 
-    should(callback.called).be.equal(true);
-    callback.calledOnce.should.be.equal(true);
-    callback.callCount.should.be.eql(1);
+      should(callback.called).be.equal(true);
+      callback.calledOnce.should.be.equal(true);
+      callback.callCount.should.be.eql(1);
+    });
   });
 
-  it('calls callback', function() {
+  describe('User', function() {
+
+    var user;
+
+    beforeEach(function() {
+      user = createUser();
+      sinon.spy(user, 'register');
+      sinon.spy(user, 'sendMail');
+    });
+
+    afterEach(function() {
+      user.register.restore();
+    })
+    ;
     
+    it('has property and its value is null when initialized ', function() {
+      user.should.have.property("email");
+      (user.email === null).should.be.equal(true);
+    });
+
+    it('should fill email property when register method is called', function() {
+      (user.email === null).should.be.equal(true);
+      user.register('lambda@domain.com');
+      user.email.should.not.equal(null);
+      user.register.calledOnce.should.eql(true);
+      user.register.calledWith("lambda@domain.com").should.eql(true);
+    });
+
+    it('should call the callback passing error object when email is null', function() {
+      var callback = sinon.spy(function(err) {
+	throw err;
+      });
+      
+      try {
+	user.sendMail('help@domain.com', callback);
+      } catch (err) {}
+      callback.calledOnce.should.eql(true);
+      callback.calledWith(new Error()).should.eql(true);
+      user.sendMail.calledOn(user).should.eql(true);
+      user.sendMail.threw().should.be.eql(true);
+      
+      try {
+	user.sendMail('help@domain.com', callback);
+      } catch (err) {}
+      user.sendMail.calledTwice.should.equal(true);
+      user.sendMail.alwaysThrew().should.be.eql(true);
+      callback.calledOnce.should.eql(false); // called twice, so false
+    });
   });
 });
